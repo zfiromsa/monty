e0196fa37f92565eaade05b68ea49d7dd3018337
zfiromsa <mdzm841@gmail.com>
2023-12-14 05:04:57 -0800

    go

--- pint.c
+++ pint.c
@@ -4,11 +4,7 @@ void F_pint(stack_t **stack, unsigned int line_number)
 {
 stack_t *tmp;
 
-tmp = *stack;
 (void)line_number;
-while (tmp)
-{
-tmp = tmp->next;
+printf("%d\n",(*stack)->n);
 }
-printf("%d\n", tmp->n);
-}
+
