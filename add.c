f7c0f9114ae83714dd94bea6da00eeaad0d5dc5c
zfiromsa <mdzm841@gmail.com>
2023-12-16 04:22:52 -0800

    go

--- add.c
+++ add.c
@@ -3,25 +3,25 @@
 void F_add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-    int i;
+	int i;
 
-    i = 0;
-    tmp = (*stack);
+	i = 0;
+	tmp = (*stack);
 	while (tmp)
 	{
-        i++;
-        tmp = tmp->next;
-    }
-    if (i < 2)
-    {
+		i++;
+		tmp = tmp->next;
+	}
+	if (i < 2)
+	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		free_stack(stack);
 		free(all_key.content);
 		fclose(all_key.file);
 		exit(EXIT_FAILURE);
 	}
-    tmp->prev->n = tmp->n + (tmp->prev->n);
-    tmp->prev->next = NULL;
-    free(tmp);
+	tmp->prev->n = tmp->n + (tmp->prev->n);
+	tmp->prev->next = NULL;
+	free(tmp);
 }
 
