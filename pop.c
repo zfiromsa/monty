3240fa30bca40b752acba0f2aa31775654bbe4c6
zfiromsa <mdzm841@gmail.com>
2023-12-16 05:33:30 -0800

    go

--- pop.c
+++ pop.c
@@ -4,7 +4,7 @@ void F_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 
-	if (!(*stack))
+	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free_stack(stack);
